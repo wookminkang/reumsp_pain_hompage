0f04579fb9ed2187bbc54d45044fe4da860b8ed9
kangminwook <kangmu950@gmail.com>
2026-09-08 03:06:11 +0000

    chore: 칼럼 예약 발행 (seongnae-car-accident-treatment)

--- content/columns/index.ts
+++ content/columns/index.ts
@@ -39,6 +39,7 @@ import { neckStiffnessHandNumbness } from "./neck-stiffness-hand-numbness";
 import { postExercisePain } from "./post-exercise-pain";
 import { radiationTherapyRecovery } from "./radiation-therapy-recovery";
 import { samsungHospitalNearbyNursingHospital } from "./samsung-hospital-nearby-nursing-hospital";
+import { seongnaeCarAccidentTreatment } from "./seongnae-car-accident-treatment";
 import { seongnaeNeckPainTreatment } from "./seongnae-neck-pain-treatment";
 import { seongsimHospitalNearbyKoreanMedicine } from "./seongsim-hospital-nearby-korean-medicine";
 import { seoulPainHospitalRecommendation } from "./seoul-pain-hospital-recommendation";
@@ -102,6 +103,7 @@ export const COLUMNS: ColumnArticle[] = [
   postExercisePain,
   radiationTherapyRecovery,
   samsungHospitalNearbyNursingHospital,
+  seongnaeCarAccidentTreatment,
   seongnaeNeckPainTreatment,
   seongsimHospitalNearbyKoreanMedicine,
   seoulPainHospitalRecommendation,

4f0397014fdf973ff849ee3fb847f96c28f31bc0
kangminwook <kangmu950@gmail.com>
2026-09-10 03:08:12 +0000

    chore: 칼럼 예약 발행 (bangi-car-accident-aftereffects)

--- content/columns/index.ts
+++ content/columns/index.ts
@@ -1,6 +1,7 @@
 import type { ColumnArticle } from "@/lib/columns/types";
 import { asanHospitalNearbyKoreanMedicine } from "./asan-hospital-nearby-korean-medicine";
 import { asanHospitalNearbyNursingHospital } from "./asan-hospital-nearby-nursing-hospital";
+import { bangiCarAccidentAftereffects } from "./bangi-car-accident-aftereffects";
 import { bangiPelvicPainTreatment } from "./bangi-pelvic-pain-treatment";
 import { bohunHospitalNearbyNursingHospital } from "./bohun-hospital-nearby-nursing-hospital";
 import { breastCancerSurgeryRecovery } from "./breast-cancer-surgery-recovery";
@@ -66,6 +67,7 @@ import { thyroidCancerSurgeryRecovery } from "./thyroid-cancer-surgery-recovery"
 export const COLUMNS: ColumnArticle[] = [
   asanHospitalNearbyKoreanMedicine,
   asanHospitalNearbyNursingHospital,
+  bangiCarAccidentAftereffects,
   bangiPelvicPainTreatment,
   bohunHospitalNearbyNursingHospital,
   breastCancerSurgeryRecovery,

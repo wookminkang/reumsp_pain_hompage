914eda425750388a178e5da4bc09c906297d3adb
kangminwook <kangmu950@gmail.com>
2026-09-22 03:21:33 +0000

    chore: 칼럼 예약 발행 (macheon-car-accident-korean-medicine-clinic)

--- content/columns/index.ts
+++ content/columns/index.ts
@@ -48,6 +48,7 @@ import { konkukHospitalNearbyKoreanMedicine } from "./konkuk-hospital-nearby-kor
 import { konkukHospitalNearbyNursingHospital } from "./konkuk-hospital-nearby-nursing-hospital";
 import { kyungheeHospitalNearbyKoreanMedicine } from "./kyunghee-hospital-nearby-korean-medicine";
 import { kyungheeHospitalNearbyNursingHospital } from "./kyunghee-hospital-nearby-nursing-hospital";
+import { macheonCarAccidentKoreanMedicineClinic } from "./macheon-car-accident-korean-medicine-clinic";
 import { myeongilCarAccidentTreatment } from "./myeongil-car-accident-treatment";
 import { neckStiffnessHandNumbness } from "./neck-stiffness-hand-numbness";
 import { ogeumCarAccidentKoreanMedicineClinic } from "./ogeum-car-accident-korean-medicine-clinic";
@@ -129,6 +130,7 @@ export const COLUMNS: ColumnArticle[] = [
   konkukHospitalNearbyNursingHospital,
   kyungheeHospitalNearbyKoreanMedicine,
   kyungheeHospitalNearbyNursingHospital,
+  macheonCarAccidentKoreanMedicineClinic,
   myeongilCarAccidentTreatment,
   neckStiffnessHandNumbness,
   ogeumCarAccidentKoreanMedicineClinic,
